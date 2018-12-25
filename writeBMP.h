983417642134226d623c2057dd5dcdf16e78bf95
shiloyair88 <45846543+shiloyair88@users.noreply.github.com>
2018-12-25 19:25:59 +0200

    Delete writeBMP.h

--- writeBMP.h
+++
@@ -1,8 +0,0 @@
-#ifndef WRITE_BMP_H_
-#define WRITE_BMP_H_
-
-#include "readBMP.h"
-
-void writeBMP(Image *image, const char* originalImgFileName, const char* fileName);
-void writeBMP2(Image *image, const char* originalImgFileName, const char* fileName);
-#endif /* WRITE_BMP_H_ */
