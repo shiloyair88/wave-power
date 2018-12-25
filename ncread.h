f409a2cf687df6a4e469d90223bf202000662229
shiloyair88 <45846543+shiloyair88@users.noreply.github.com>
2018-12-25 19:25:29 +0200

    Delete ncread.h

--- ncread.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "interpolation.h"
-size_t read(const char *filename, const char *varname, point **dataout);
-size_t *read2(const char *filename, const char *varname, point **dataout)
